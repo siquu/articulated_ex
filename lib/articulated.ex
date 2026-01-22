312cf720978e88bb8c17e2b6a2bcec425aa30afd
siquu <186956095+siquu@users.noreply.github.com>
2026-01-21 23:11:59 -0500

    init

---
+++ lib/articulated.ex
@@ -0,0 +1,18 @@
+defmodule Articulated do
+  @moduledoc """
+  Documentation for `Articulated`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Articulated.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/articulated_test.exs
@@ -0,0 +1,8 @@
+defmodule ArticulatedTest do
+  use ExUnit.Case
+  doctest Articulated
+
+  test "greets the world" do
+    assert Articulated.hello() == :world
+  end
+end
